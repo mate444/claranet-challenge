175be48139a463edd3e659d2c94a52fa411f1df7
mate444 <mateoeo23@gmail.com>
2024-01-25 13:34:45 +0100

    Adds useSearch custom hook

---
+++ src/hooks/useSearch.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useSetRecoilState } from "recoil";
+import { VideosAtom } from "../state/Videos";
+
+
+export default function useSearch () {
+  const setVideos = useSetRecoilState(VideosAtom);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const url = "https://www.googleapis.com/youtube/v3/search";
+    return async (search: string) => {
+      const response = await axios.get(`${url}?key=${apiKey}&q=${search}&maxResults=50&part=snippet&type=video`);
+      const { data } = response;
+      setVideos(data.items);
+      console.log("Request sent");
+    };
+}
